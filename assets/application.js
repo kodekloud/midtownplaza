5407c100049f89607325e311a7f712f0cdfeff0f
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-17 12:00:55 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -64,7 +64,7 @@ function renderStoreList(container, template, collection, type){
         else{
             val.initial = current_initial;
             store_initial = current_initial;
-            val.show = "display:block;";
+            val.show = "display:inline-block;";
         }
         if (val.promotions.length > 0){
             val.promotion_exist = "display:inline-block";
