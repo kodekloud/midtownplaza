082374a0ff40110e95bf688bec675da8c41c1bf8
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-17 12:07:06 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -65,7 +65,7 @@ function renderStoreList(container, template, collection, type){
             val.initial = current_initial;
             store_initial = current_initial;
             if (val.initial = "A"){
-                val.show = "display:inline-block;";
+                val.show = "display:inline-block;margin-top:24px";
             }
             else{
                 val.show = "display:inline-block;";
