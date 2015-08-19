7d4a48e2624beb3f8c56cfead60450a09371f9b1
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-19 16:27:29 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -350,6 +350,11 @@ function renderPromosEvents(container, template, collection){
         if ((val.image_url).indexOf('missing.png') > -1) {
             val.image_url = "http://assets.kodekloud.io/sites/55bba30d6e6f64157e000000/24ac5b317a383812fad7eab38651125a/mp_logo_2.png";
         }
+        if (start.toDateString() == end.toDateString()) {
+                val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
+            } else {
+                val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
+            }
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
