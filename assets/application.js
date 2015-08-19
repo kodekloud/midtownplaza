5470ac52256291b32a93c7a027520ef9469febf8
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-19 15:23:55 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -341,6 +341,8 @@ function renderPromosEvents(container, template, collection){
     var template_html = $(template).html();
     Mustache.parse(template_html);   // optional, speeds up future use
     $.each( collection , function( key, val ) {
+        var rendered = Mustache.render(template_html,val);
+         item_rendered.push(rendered);
     });
 }
 
