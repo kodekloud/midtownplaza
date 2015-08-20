4e915d077d0bfe9844b6a469132fa67742033fa8
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-20 13:36:45 -0400

    via kodekloud.io (55bba3d26e6f6417b5000000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -7,7 +7,7 @@ function show_content(){
     $(".modal-backdrop").remove();
 }
 
-function pinIt(e){
+function pinIt(event){
     e.preventDefault();
     var e = document.createElement('script');
     e.setAttribute('type','text/javascript');
