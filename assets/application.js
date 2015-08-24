f48b13bcd594bd2881d88d1b5166ba960d0f9f95
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-24 12:02:42 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -406,7 +406,9 @@ function renderPromoDetails(container, template, collection){
             }
             val.alt_promo_image_url = getImageURL(val.promo_image_url);
         }
-        
+        if (val.store_name.length <= 0){
+            val.store_name = "Midtown Plaza"
+        }
         start = new Date (val.start_date);
         end = new Date (val.end_date);
         start.setDate(start.getDate()+1);
