eb59b745069c2f7569dd206cebcd6885d2eef7f8
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-24 11:56:25 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -387,7 +387,7 @@ function renderPromoDetails(container, template, collection){
                 val.alt_promo_image_url = getImageURL(store_details.store_front_url);
                 val.store_detail_btn = store_details.slug ;
                 val.store_name = store_details.name;
-                console.log("here")
+                
                 
             } else {
                 val.alt_promo_image_url = "http://assets.kodekloud.io/sites/55bba30d6e6f64157e000000/24ac5b317a383812fad7eab38651125a/mp_logo_2.png";
