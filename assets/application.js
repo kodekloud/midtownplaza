ec495d19f9b22001320f9eec1dd4e6f904c572b9
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-24 11:20:20 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -57,6 +57,7 @@ function renderStoreList(container, template, collection, type){
         //var categories = getStoreCategories();
         var current_initial = val.name[0];
         val.categories_list = getCategoryDetails(val.categories[0])
+        console.log(categories_list)
         if(store_initial.toLowerCase() == current_initial.toLowerCase()){
             val.initial = "";
             val.show = "display:none;";
