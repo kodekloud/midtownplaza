c593e29c8272f28a1b3607ec03fa4583c76a706c
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-24 11:36:19 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -120,6 +120,7 @@ function renderStoreDetails(container, template, collection, slug){
         if(val.description.length > 0){
             val.desc_pad = "padding_10";
         }
+        console.log(val)
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
