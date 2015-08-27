9022a6c50ab492f42cba93d778925e9a242ae417
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-27 11:13:47 -0400

    via kodekloud.io (55bba3d26e6f6417b5000000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -31,7 +31,7 @@ function init(e){
     var collapse_contact = ["/jobs", "/pages/midtown2-contact-us", "/pages/midtown2-leasing"];
     
     if ($.inArray(path, collapse_shopping) >= 0){
-        $('#dropdownMenu1').collapse('toggle')
+        $('#dropdownMenu1').addClass('active_menu')
     }
 }
 
