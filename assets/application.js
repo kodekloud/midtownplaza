8409fd5bdff326952009830f76c78889f8add9f2
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-31 16:16:38 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -702,6 +702,28 @@ function init_side(){
     renderPosts("#home_blog_container", "#home_blog_template", list);
 }
 
+function renderHomeHours(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    item_list.push(collection);    
+    $.each( item_list , function( key, val ) {
+        val.day = get_day(val.day_of_week)
+        if (val.open_time && val.close_time && (val.is_closed == false || val.is_closed == null)){
+            var open_time = new Date (val.open_time)
+            var close_time = new Date (val.close_time)
+            val.open_time = convert_hour(open_time);
+            val.close_time = convert_hour(close_time);    
+            val.h = val.open_time+ " - " + val.close_time;
+        } else {
+            val.h = "Closed"
+        }
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
 
 
 
