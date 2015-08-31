9241d77b900c3dd4f8ba5f28dce4b81e6e19fce4
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-31 15:18:38 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -691,7 +691,7 @@ function renderPostDetails(container, template, collection){
 function init_side_blog(){
     var posts = getAllPublishedPosts();
     var published_posts = posts.sortBy(function(o){ return new Date(o.publish_date) }).reverse();
-    renderPosts("#home_blog_container", "#blog_template", published_posts);
+    renderPosts("#home_blog_container", "#home_blog_template", published_posts);
 }
 
 
