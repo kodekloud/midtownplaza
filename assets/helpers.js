e6507cd615ecf9fa133ba0c4a91b997ca15573b4
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-08-31 16:14:42 -0400

    via kodekloud.io (55bba3d26e6f6417b5000000)

--- assets/helpers.js
+++ assets/helpers.js
@@ -62,30 +62,30 @@ function pinIt(){
 }
 
 function get_day(id){
-        switch(id) {
-            case 0:
-                return ("Sunday")
-                break;
-            case 1:
-                return ("Monday")
-                break;
-            case 2:
-                return ("Tuesday")
-                break;
-            case 3:
-                return ("Wednesday")
-                break;
-            case 4:
-                return ("Thursday")
-                break;
-            case 5:
-                return ("Friday")
-                break;
-            case 6:
-                return ("Saturday")
-                break;
-        }
+    switch(id) {
+        case 0:
+            return ("Sunday")
+            break;
+        case 1:
+            return ("Monday")
+            break;
+        case 2:
+            return ("Tuesday")
+            break;
+        case 3:
+            return ("Wednesday")
+            break;
+        case 4:
+            return ("Thursday")
+            break;
+        case 5:
+            return ("Friday")
+            break;
+        case 6:
+            return ("Saturday")
+            break;
     }
+}
 
 
 function convert_hour(d){
