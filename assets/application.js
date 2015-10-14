6207c5895488b96f91b88be6b8fb5fb3f680a599
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-10-14 17:29:20 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -483,7 +483,6 @@ function renderPromoDetails(container, template, collection){
        
         start = new Date (val.start_date + "T05:00:00Z");
         end = new Date (val.end_date + "T05:00:00Z");
-        start.setDate(start.getDate()+1);
         if (start.toDateString() == end.toDateString()) {
             val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
         } else {
