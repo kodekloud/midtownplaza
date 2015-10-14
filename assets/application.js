bd07b3c4ccf0f9f2457af5a471a47c365d8b1a38
rajbir@mobilefringe.com <rajbir@mobilefringe.com>
2015-10-14 13:39:32 -0400

    via kodekloud.io (55bba30d6e6f64157e050000)

--- assets/application.js
+++ assets/application.js
@@ -481,10 +481,9 @@ function renderPromoDetails(container, template, collection){
         }
         console.log(val.url)
        
-        start = new Date (val.start_date);
-        end = new Date (val.end_date);
+        start = new Date (val.start_date + "T05:00:00Z");
+        end = new Date (val.end_date + "T05:00:00Z");
         start.setDate(start.getDate()+1);
-        end.setDate(end.getDate()+1);
         if (start.toDateString() == end.toDateString()) {
             val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
         } else {
@@ -529,10 +528,8 @@ function renderEventDetails(container, template, collection){
             val.store_name = "Midtown Plaza";
             
         }
-        start = new Date (val.start_date);
-        end = new Date (val.end_date);
-        start.setDate(start.getDate()+1);
-        end.setDate(end.getDate()+1);
+        start = new Date (val.start_date + "T05:00:00Z");
+        end = new Date (val.end_date + "T05:00:00Z");
         if (start.toDateString() == end.toDateString()) {
             val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
         } else {
